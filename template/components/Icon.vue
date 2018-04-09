e67b4c7cc1068cec6192b2f9c10998fd10f9fb49
m.b <mbajur@users.noreply.github.com>
2018-04-09 14:14:45 +0200

    Create Icon.vue

---
+++ template/components/Icon.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  svg(:class="cssClass")
+    use(:xlink:href="xlinkHref")
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  created () {
+    require(`~/assets/icons/${this.name}.svg`)
+  },
+  computed: {
+    cssClass () {
+      return [
+        'icon',
+        `icon-${this.name}`
+      ]
+    },
+    xlinkHref () {
+      return `#${this.name}`
+    }
+  }
+}
+</script>
